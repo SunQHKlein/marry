f2a17b53ec397ee96db9cb7ae515d74ffed4db7e
yourname <sunqinghuai@4paradigm.com>
2019-01-05 10:07:18 +0800

    first commit

---
+++ src/main/java/com/mtm/party/user/service/UserService.java
@@ -0,0 +1,13 @@
+package com.mtm.party.user.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author qubin
+ * @date 2017-07-21
+ */
+
+@Service
+public class UserService {
+
+}
---
+++ src/test/java/com/mtm/party/party/PartyApplicationTests.java
@@ -0,0 +1,17 @@
+package com.mtm.party.party;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PartyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
